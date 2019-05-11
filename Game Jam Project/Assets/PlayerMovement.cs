84c29ff586cda53bdf4c0027a1fd3c26254aac41
DESKTOP-RV5IBVV\aLEX <postualexandru@yahoo.com>
2019-05-11 14:08:46 +0300

    Camera cat de cat smooth, ar mai merge schimbat modul in care ma deplasez

---
+++ Game Jam Project/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float carSpeed = 1000000;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            transform.position -= new Vector3(carSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            transform.position += new Vector3(carSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            transform.position += new Vector3(0, carSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            transform.position -= new Vector3(0, carSpeed * Time.deltaTime, 0);
+        }
+    }
+}
